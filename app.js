7f9a33797cef9bbb5e89800eb341c2bba948d641
ChadBonstein <chad.bonstein@gmail.com>
2023-11-08 18:46:13 -0500

    Added reset and auto clicker purchase button

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
+
+    
 let clicks = 0;
+let autoClick=0;
 let donutButton = document.getElementById("donut");
+let resetButton = document.getElementById("reset");
+let autoClickButton = document.getElementById("autoclick");
 let counter = document.getElementById("counter");
 function increment(){
         clicks = clicks + 1;
         counter.innerHTML = clicks;
 }
+function purchaseAutoClicker(){
+    if (clicks >=100){
+        clicks -=100
+        counter.innerHTML = clicks;
+        autoClick = autoClick + 1
+        setInterval(() => {
+            clicks = clicks + autoClick
+            counter.innerHTML = clicks;
+        }, 1000);
+    }
+}
 function reset(){
         clicks = 0;
+        autoClick = 0;
         counter.innerHTML = clicks;
 }
 donutButton.addEventListener("click", increment);
+autoClickButton.addEventListener("click",purchaseAutoClicker);
+resetButton.addEventListener("click",reset)
+
